595e9274b6eeb965e6e28e16a922bc5a3fafe9ea
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-04-27 11:27:53 +0800

    Update orderController.js

--- controllers/products/orderController.js
+++ controllers/products/orderController.js
@@ -15,7 +15,7 @@ exports.showOrders = async (req, res) => {
 
 // for api access on different apps--------------
 exports.showOrdersApi = async (req, res) => {
-    const findOrders = await Order.find().populate('product');
+    const order = await Order.find().populate('product');
 
     res.json({ order });
 };
